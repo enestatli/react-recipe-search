0b440633a8a5aedfa7508883214620323939920a
enestatli <eenestatli@gmail.com>
2019-07-08 20:53:34 +0300

    Updates

---
+++ precache-manifest.1dd3172ce4f098d6b86a2a69cdb0627f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1ff936a9394fbcbf925e392157d7454",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46a407bebc94a5bb6557",
+    "url": "/static/js/2.86ed76bb.chunk.js"
+  },
+  {
+    "revision": "f5f0348b46bf12e6868d",
+    "url": "/static/js/main.a5bf6ec1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
